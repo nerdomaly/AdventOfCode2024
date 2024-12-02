1396b0d0fd1064f20d3a2639e25e917d969ab10c
Robert White <robwwhite@gmail.com>
2024-12-02 13:28:47 -0500

    day02 pt2 python

--- day02/day02.py
+++ day02/day02.py
@@ -1,4 +1,11 @@
-totalSafe = 0
+totalSafeNoDampener = 0
+totalSafeWithDampener = 0
+
+def isSorted(row):
+    sortedAsc = sorted(row, key=int)
+    sortedDesc = sorted(row, key=int, reverse=True)
+
+    return row == sortedAsc or row == sortedDesc
 
 def safePairs(pairs):
     safeArray = list(map(lambda x: True if abs(x[0] - x[1]) >= 1 and abs(x[0] - x[1]) <= 3 else False, pairs))
@@ -9,12 +16,16 @@ with open('input.txt') as fh:
     for line in fh:
         row = list(map(int, line.split()))
 
-        sortedAsc = sorted(row, key=int)
-        sortedDesc = sorted(row, key=int, reverse=True)
+        if (isSorted(row) and safePairs(zip(row, row[1:]))):
+            totalSafeNoDampener += 1
+        else:
+            for index in range(len(row)):
+                dampened = [x for i, x in enumerate(row) if i != index]
 
-        if (row == sortedAsc or row == sortedDesc):
-            safe = safePairs(zip(row, row[1:]))
+                if (isSorted(dampened)) and safePairs(zip(dampened, dampened[1:])):
+                    totalSafeWithDampener += 1
+                    break
 
-            totalSafe += 1 if safe else 0
-
-print("Total Safe:", totalSafe)
+print("Total Safe No Dampener:", totalSafeNoDampener)
+print("Total Safe Dampener:", totalSafeWithDampener)
+print("Total Safe:", totalSafeNoDampener + totalSafeWithDampener)
