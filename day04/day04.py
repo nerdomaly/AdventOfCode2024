fc7fce46579c981d7088d3b50f985522ef9d812b
Robert White <robwwhite@gmail.com>
2024-12-04 09:55:07 -0500

    day04 pt2 python

--- day04/day04.py
+++ day04/day04.py
@@ -1,31 +1,43 @@
-solution = "XMAS"
-totalCount = 0
+solution1 = "XMAS"
+solution2 = "MAS"
+totalSolution1 = 0
+totalSolution2 = 0
 
-def checkSolution(string):
+def checkSolution(string, solution):
     return string == solution or string[::-1] == solution
-
 
 puzzle = open('input.txt').readlines()
 
 for y in range(len(puzzle)):
     puzzleLine = puzzle[y].strip()
-    print(f'puzzle line [{y}]:', puzzleLine)
+    # print(f'puzzle line [{y}]:', puzzleLine)
 
     for x in range(len(puzzleLine)):
+        # Check solution 1:
+        # Looking for XMAS across, down, backslash, and forwardslash
         if (x + 3 < len(puzzleLine)):
             across = puzzleLine[x:x+4]
-            totalCount += 1 if checkSolution(across) else 0
+            totalSolution1 += 1 if checkSolution(across, solution1) else 0
 
         if (y + 3 < len(puzzle)):
             down = ''.join(l for l in [puzzle[y][x], puzzle[y+1][x], puzzle[y+2][x], puzzle[y+3][x]])
-            totalCount += 1 if checkSolution(down) else 0
+            totalSolution1 += 1 if checkSolution(down, solution1) else 0
 
         if (y + 3 < len(puzzle) and x + 3 < len(puzzleLine)):
             backslash = ''.join(l for l in [puzzle[y][x], puzzle[y+1][x+1], puzzle[y+2][x+2], puzzle[y+3][x+3]])
-            totalCount += 1 if checkSolution(backslash) else 0
+            totalSolution1 += 1 if checkSolution(backslash, solution1) else 0
 
         if (y + 3 < len(puzzle) and x - 3 >= 0):
             forwardslash = ''.join(l for l in [puzzle[y][x], puzzle[y+1][x-1], puzzle[y+2][x-2], puzzle[y+3][x-3]])
-            totalCount += 1 if checkSolution(forwardslash) else 0
+            totalSolution1 += 1 if checkSolution(forwardslash, solution1) else 0
         
-print("Total Count:", totalCount)
+        # Check solution 2:
+        # Looking for MAS in X pattern
+        if (y + 2 < len(puzzle) and x + 2 < len(puzzleLine)):
+            backslash = ''.join(l for l in [puzzle[y][x], puzzle[y+1][x+1], puzzle[y+2][x+2]])
+            forwardslash = ''.join(l for l in [puzzle[y+2][x], puzzle[y+1][x+1], puzzle[y][x+2]])
+
+            totalSolution2 += 1 if checkSolution(backslash, solution2) and checkSolution(forwardslash, solution2) else 0
+            
+print("Total Soution 1:", totalSolution1)
+print("Total Soution 2:", totalSolution2)
